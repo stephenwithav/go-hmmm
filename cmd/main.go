6f1bbac5ef9da7bde596672cc57a4e53c1d5c5a3
Steven Edwards <steven@stephenwithav.com>
2020-11-15 20:49:40 -0500

    Complete TUI alpha.
    The TUI successfully retrieves, stars, and saves selected topics from
    arXiv categories to text files.

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	ui "github.com/gizak/termui/v3"
 	"github.com/gizak/termui/v3/widgets"
@@ -46,61 +47,101 @@ func main() {
 		log.Fatal(err)
 	}
 
+	papersListFormat := "Papers from [%s] - [%d/%d]"
+	starsListFormat := "Papers from [%s] - [%d/%d]"
+
 	papersList := widgets.NewList()
-	papersList.Title = fmt.Sprintf("Papers from [%s]", section)
 	papersList.WrapText = false
-	papersList.SetRect(0, 0, 40, 40)
 	papersList.TextStyle = ui.NewStyle(ui.ColorWhite)
 	papersList.SelectedRowStyle = ui.NewStyle(ui.ColorBlue)
 	papersList.Rows = paperTitles(papers)
+	papersList.Title = fmt.Sprintf(papersListFormat, section, papersList.SelectedRow+1, len(papersList.Rows))
 
 	starsList := widgets.NewList()
-	starsList.Title = fmt.Sprintf("Starred [%s] papers", section)
 	starsList.WrapText = false
-	starsList.SetRect(0, 0, 40, 40)
 	starsList.TextStyle = ui.NewStyle(ui.ColorWhite)
 	starsList.Rows = []string{}
+	starsList.Title = fmt.Sprintf(starsListFormat, section, 0, 0)
+
+	rowsToExtract := []int{}
 
 	grid := ui.NewGrid()
 	termWidth, termHeight := ui.TerminalDimensions()
 	grid.SetRect(0, 0, termWidth, termHeight)
 
 	grid.Set(
-		ui.NewRow(0.5, papersList),
-		ui.NewRow(0.5, starsList),
+		ui.NewRow(0.75, papersList),
+		ui.NewRow(0.25, starsList),
 	)
 
 	ui.Render(grid)
 
 	previousKey := ""
 	uiEvents := ui.PollEvents()
+	activeList := papersList
 	for {
 		e := <-uiEvents
 		switch e.ID {
 		case "q", "<C-c>":
 			return
 		case "j", "<Down>":
-			papersList.ScrollDown()
+			activeList.ScrollDown()
 		case "k", "<Up>":
-			papersList.ScrollUp()
+			activeList.ScrollUp()
 		case "<C-d>":
-			papersList.ScrollHalfPageDown()
+			activeList.ScrollHalfPageDown()
 		case "<C-u>":
-			papersList.ScrollHalfPageUp()
+			activeList.ScrollHalfPageUp()
 		case "<C-f>":
-			papersList.ScrollPageDown()
+			activeList.ScrollPageDown()
 		case "<C-b>":
-			papersList.ScrollPageUp()
+			activeList.ScrollPageUp()
 		case "g":
 			if previousKey == "g" {
-				papersList.ScrollTop()
+				activeList.ScrollTop()
 			}
 		case "<Home>":
-			papersList.ScrollTop()
+			activeList.ScrollTop()
 		case "G", "<End>":
-			papersList.ScrollBottom()
+			activeList.ScrollBottom()
 		case "s":
-			starsList.Rows = append(starsList.Rows, papersList.Rows[papersList.SelectedRow])
+			if activeList == papersList {
+				starsList.Rows = append(starsList.Rows, papersList.Rows[papersList.SelectedRow])
+				rowsToExtract = append(rowsToExtract, papersList.SelectedRow)
+				starsList.ScrollBottom()
+			}
+		case "u":
+			if activeList == papersList {
+				break
+			}
+			if starsList.SelectedRow == 0 {
+				starsList.Rows = starsList.Rows[1:]
+				rowsToExtract = rowsToExtract[1:]
+			} else {
+				rowsToExtract = append(rowsToExtract[0:starsList.SelectedRow], rowsToExtract[starsList.SelectedRow+1:]...)				
+				starsList.Rows = append(starsList.Rows[0:starsList.SelectedRow], starsList.Rows[starsList.SelectedRow+1:]...)				
+			}
+		case "<C-i>", "<Tab>":
+			if activeList == papersList {
+				papersList.SelectedRowStyle = ui.NewStyle(ui.ColorWhite)
+				starsList.SelectedRowStyle = ui.NewStyle(ui.ColorBlue)
+				activeList = starsList
+			} else {
+				starsList.SelectedRowStyle = ui.NewStyle(ui.ColorWhite)
+				papersList.SelectedRowStyle = ui.NewStyle(ui.ColorBlue)
+				activeList = papersList
+			}
+		case "<C-e>": // Extract
+			w, err:=os.Create(time.Now().Format("2006-01-02") + "-" + section + ".html")
+			if err != nil {
+				break
+			}
+			fmt.Fprint(w, "<html><body><textarea>\n")
+			for _, row := range rowsToExtract {
+				fmt.Fprintf(w, "%s\n\n%s\n\n", papers[row], papers[row].ScienceWiseURL())
+			}
+			fmt.Fprint(w, "\n</textarea></body></html>")
+			w.Close()
 		}
 
 		if previousKey == "g" {
@@ -109,6 +150,8 @@ func main() {
 			previousKey = e.ID
 		}
 
+		papersList.Title = fmt.Sprintf(papersListFormat, section, papersList.SelectedRow+1, len(papersList.Rows))
+		starsList.Title = fmt.Sprintf(starsListFormat, section, starsList.SelectedRow+1, len(starsList.Rows))
 		ui.Render(grid)
 	}
 }
